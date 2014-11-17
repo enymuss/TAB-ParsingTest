7afb5a66584423e0849198899538f7b7a8839cf2
Jolanta Karwowska <jolantakarwowska@MacBook-2.local>
2014-11-17 20:23:09 +0000

    Added Hpple

---
+++ TAB-Parsing/Team Memeber.swift
@@ -0,0 +1,16 @@
+//
+//  Team Memeber.swift
+//  TAB-Parsing
+//
+//  Created by Jolanta Karwowska on 17/11/2014.
+//  Copyright (c) 2014 Richard Szczerba. All rights reserved.
+//
+
+import Foundation
+
+class TeamMember {
+  
+  var nameAndSurname : String = ""
+  var positionInCompany : String = ""
+  var profileImageURL : String = ""
+}
--- TAB-Parsing/ViewController.swift
+++ TAB-Parsing/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
 
+  
+  @IBOutlet var teamMembersCollectionView: UICollectionView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
