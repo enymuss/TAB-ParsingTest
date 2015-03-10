4ba1432c89d5214ca9f61fc4dd33c9279d034a71
Richard Szczerba <richardszczerba@gmail.com>
2015-03-10 22:46:40 +0000

    Add Cocoapods
    Add Cocoapods

---
+++ Pods/Target Support Files/Pods-TAB-Parsing-hpple/Pods-TAB-Parsing-hpple-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TAB_Parsing_hpple : NSObject
+@end
+@implementation PodsDummy_Pods_TAB_Parsing_hpple
+@end
--- Pods/hpple/XPathQuery.h
+++ Pods/hpple/XPathQuery.h
@@ -6,9 +6,5 @@
 //  Copyright 2008 __MyCompanyName__. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-
 NSArray *PerformHTMLXPathQuery(NSData *document, NSString *query);
-NSArray *PerformHTMLXPathQueryWithEncoding(NSData *document, NSString *query,NSString *encoding);
 NSArray *PerformXMLXPathQuery(NSData *document, NSString *query);
-NSArray *PerformXMLXPathQueryWithEncoding(NSData *document, NSString *query,NSString *encoding);
